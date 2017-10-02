d093fc100a7d105adc003460c98a4dfe9c837a21
Serhii Kutsyi <sergey.kutcyi@gmail.com>
2017-10-02 22:00:05 +0300

    Initial commit

---
+++ src/main/java/com/serhiikutsyi/stock/StockAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.serhiikutsyi.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalysisApplication.class, args);
+	}
+}
