f596ead9009e478b95e01aa35044025b6042418c
Serhii Kutsyi <sergey.kutcyi@gmail.com>
2017-10-23 16:50:38 +0300

    Fix dashboard app build

---
+++ stock-dashboard/src/main/java/com/serhiikutsyi/stock/dashboard/DashboardApplication.java
@@ -0,0 +1,16 @@
+package com.serhiikutsyi.stock.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Serhii Kutsyi
+ */
+@SpringBootApplication
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+
+}
